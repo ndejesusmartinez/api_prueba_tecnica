bb9f4993039caf427e072799a864232471f4a8b6
ndejesusmartinez <nmartinezc97@hotmail.com>
2024-05-18 12:19:05 -0500

    Implementación de autenticación parte #2 y proteccion de rutas

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -26,5 +27,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof AuthenticationException) {
+            return response()->json(['error' => 'Not Authorized, validate token'], 401);
+        }
+
+        return parent::render($request, $e);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,27 +24,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('products')->group(function () {
     Route::get('listPrices', [ProductController::class, 'getPrices']);
-    Route::post('', [ProductController::class, 'store']);
-    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::post('', [ProductController::class, 'store'])->middleware('auth:api');
+    Route::put('/{id}', [ProductController::class, 'update'])->middleware('auth:api');
     Route::get('', [ProductController::class, 'index']);
-    Route::delete('/{id}', [ProductController::class, 'destroy']);
+    Route::delete('/{id}', [ProductController::class, 'destroy'])->middleware('auth:api');
     Route::get('/{id}', [ProductController::class, 'find']);
 });
 
 Route::prefix('categories')->group(function () {
-    Route::post('', [CategoriesController::class, 'store']);
-    Route::put('/{id}', [CategoriesController::class, 'update']);
+    Route::post('', [CategoriesController::class, 'store'])->middleware('auth:api');
+    Route::put('/{id}', [CategoriesController::class, 'update'])->middleware('auth:api');
     Route::get('', [CategoriesController::class, 'index']);
-    Route::delete('/{id}', [CategoriesController::class, 'destroy']);
+    Route::delete('/{id}', [CategoriesController::class, 'destroy'])->middleware('auth:api');
     Route::get('/{id}', [CategoriesController::class, 'find']);
 });
 
 Route::prefix('users')->group(function () {
     Route::post('', [UserController::class, 'store']);
-    Route::get('', [UserController::class, 'index']);
+    Route::get('', [UserController::class, 'index'])->middleware('auth:api');
     Route::get('/{id}', [UserController::class, 'find']);
-    Route::put('/{id}', [UserController::class, 'update']);
-    Route::delete('/{id}', [UserController::class, 'destroy']);
+    Route::put('/{id}', [UserController::class, 'update'])->middleware('auth:api');
+    Route::delete('/{id}', [UserController::class, 'destroy'])->middleware('auth:api');
 });
 
 Route::prefix('auth')->group(function () {
